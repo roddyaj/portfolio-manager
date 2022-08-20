fd07e8fe2fa2f2d9e1986aa049a717899a0ae089
Adam Roddy <aroddy@mail.com>
2022-08-19 18:14:18 -0600

    Hide some columns in action mode

--- client-react/src/Positions.js
+++ client-react/src/Positions.js
@@ -21,58 +21,52 @@ function Positions(props) {
 				<thead>
 					<tr>
 						<th className="l">Ticker</th>
-						<th>#</th>
+						{showAllPositions && <th>#</th>}
 						<th>Price</th>
-						<th>Value</th>
+						{showAllPositions && <th>Value</th>}
 						<th>Day</th>
-						<th>G/L</th>
-						<th>Actual</th>
-						<th>Target</th>
-						<th>Ratio</th>
+						{showAllPositions && <th>G/L</th>}
+						{showAllPositions && <th>Actual</th>}
+						{showAllPositions && <th>Target</th>}
+						{showAllPositions && <th>Ratio</th>}
 						<th className="l">Action</th>
-						<th className="l">Open</th>
+						<th className="c">Open</th>
 					</tr>
 				</thead>
 				<tbody>
-					{viewPositions.map(renderRow)}
+					{viewPositions.map((p, i) => renderRow(p, i, showAllPositions))}
 				</tbody>
 			</table>
 		</div>
 	);
 }
 
-function renderRow(position, i) {
+function renderRow(position, i, showAllPositions) {
 	const action = position.sharesToBuy > 0 ? "Buy" : "Sell";
 	const actionText = `${action} ${Math.abs(position.sharesToBuy)}`;
 	const actionUrl = `https://client.schwab.com/Areas/Trade/Allinone/index.aspx?tradeaction=${action}&Symbol=${position.symbol}`;
 	const openBuyCount = position.openOrders ? position.openOrders.filter(o => o.action === "Buy").map(o => o.quantity).reduce((tot, cur) => tot + cur, 0) : 0;
 	const openSellCount = position.openOrders ? position.openOrders.filter(o => o.action === "Sell").map(o => o.quantity).reduce((tot, cur) => tot + cur, 0) : 0;
-	let openOrderText = "";
-	if (openBuyCount) {
-		openOrderText += `B ${openBuyCount}`;
-	}
-	if (openSellCount) {
-		if (openOrderText.length) {
-			openOrderText += ", ";
-		}
-		openOrderText += `S ${openSellCount}`;
-	}
+	const openOrderArray = [['B', openBuyCount], ['S', openSellCount]].filter(a => a[1] != 0);
+	const openOrderText = showAllPositions
+		? openOrderArray.map(a => a.join(" ")).join(", ")
+		: openOrderArray.filter(a => a[0] === action.charAt(0)).map(a => a[1]).join("");
 
 	return (
 		<tr key={position.symbol}>
 			<td className="l">
 				<a href={`https://finance.yahoo.com/quote/${position.symbol}`} target="_blank" style={position.peRatio < 0 ? { backgroundColor: "#FDD" } : {}}>{position.symbol}</a>
 			</td>
-			<td>{position.quantity}</td>
+			{showAllPositions && <td>{position.quantity}</td>}
 			<td>{position.price.toFixed(2)}</td>
-			<td>{position.marketValue.toFixed(2)}</td>
+			{showAllPositions && <td>{position.marketValue.toFixed(2)}</td>}
 			<td style={{ color: position.dayChangePct >= 0 ? "green" : "#C00" }}>{Math.abs(position.dayChangePct).toFixed(2) + "%"}</td>
-			<td style={{ color: position.gainLossPct >= 0 ? "green" : "#C00" }}>{Math.abs(position.gainLossPct).toFixed(2) + "%"}</td>
-			<td>{position.percentOfAccount.toFixed(2) + "%"}</td>
-			<td>{position.targetPct ? position.targetPct.toFixed(2) + "%" : ""}</td>
-			<td>{position.targetPct ? (100 * position.percentOfAccount / position.targetPct).toFixed(1) + "%" : ""}</td>
+			{showAllPositions && <td style={{ color: position.gainLossPct >= 0 ? "green" : "#C00" }}>{Math.abs(position.gainLossPct).toFixed(2) + "%"}</td>}
+			{showAllPositions && <td>{position.percentOfAccount.toFixed(2) + "%"}</td>}
+			{showAllPositions && <td>{position.targetPct ? position.targetPct.toFixed(2) + "%" : ""}</td>}
+			{showAllPositions && <td>{position.targetPct ? (100 * position.percentOfAccount / position.targetPct).toFixed(1) + "%" : ""}</td>}
 			<td className="l">{position.sharesToBuy ? (<a href={actionUrl} target="_blank" onClick={() => copyClip(Math.abs(position.sharesToBuy))}>{actionText}</a>) : ""}</td>
-			<td className="l">
+			<td className="c">
 				{(openBuyCount || openSellCount) ? (
 					<OverlayTrigger trigger={['hover', 'focus']} placement="right" overlay={renderOpenOrdersPopover(position, i)}>
 						<span style={{ textDecoration: "underline" }}>{openOrderText}</span>
--- client-react/src/TitleBar.js
+++ client-react/src/TitleBar.js
@@ -15,11 +15,11 @@ function TitleBar(props) {
 				</div>
 
 				<div style={{ marginRight: 6 }}>
-					<span style={{ marginRight: 6 }}>Positions:</span>
-					<input type="radio" style={{ marginRight: 2 }} name="positionVisibility" id="visibility-all" value="all" checked={showAllPositions} onChange={() => setShowAllPositions(true)} />
-					<label htmlFor="visibility-all" style={{ marginRight: 6 }}>All</label>
-					<input type="radio" style={{ marginRight: 2 }} name="positionVisibility" id="visibility-actions" value="actions" checked={!showAllPositions} onChange={() => setShowAllPositions(false)} />
-					<label htmlFor="visibility-actions" style={{ marginRight: 6 }}>With actions</label>
+					<span style={{ marginRight: 6 }}>Show:</span>
+					<input type="radio" name="positionVisibility" id="visibility-all" value="all" checked={showAllPositions} onChange={() => setShowAllPositions(true)} />
+					<label htmlFor="visibility-all" style={{ padding: 2, marginRight: 4 }}>All</label>
+					<input type="radio" name="positionVisibility" id="visibility-actions" value="actions" checked={!showAllPositions} onChange={() => setShowAllPositions(false)} />
+					<label htmlFor="visibility-actions" style={{ padding: 2, marginRight: 4 }}>Actions</label>
 				</div>
 			</div>
 		</div>
